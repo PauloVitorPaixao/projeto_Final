c1a9c840d11dc4189cee53ddd9a85e5248564ed3
PauloVitorPaixao <paulovitorpro021@gmail.com>
2021-12-17 11:37:59 -0300

    att - implementação do carrinho

--- shoppingCart/src/app/pages/cart-modal/cart-modal.page.ts
+++ shoppingCart/src/app/pages/cart-modal/cart-modal.page.ts
@@ -1,15 +1,52 @@
+import { Product, CartService } from './../../services/cart.service';
 import { Component, OnInit } from '@angular/core';
-
+import { ModalController, AlertController } from '@ionic/angular';
+ 
 @Component({
   selector: 'app-cart-modal',
   templateUrl: './cart-modal.page.html',
   styleUrls: ['./cart-modal.page.scss'],
 })
 export class CartModalPage implements OnInit {
-
-  constructor() { }
-
+ 
+  cart: Product[] = [];
+ 
+  constructor(private cartService: CartService, private modalCtrl: ModalController, private alertCtrl: AlertController) { }
+ 
   ngOnInit() {
+    this.cart = this.cartService.getCart();
   }
-
+ 
+  decreaseCartItem(product) {
+    this.cartService.decreaseProduct(product);
+  }
+ 
+  increaseCartItem(product) {
+    this.cartService.addProduct(product);
+  }
+ 
+  removeCartItem(product) {
+    this.cartService.removeProduct(product);
+  }
+ 
+  getTotal() {
+    return this.cart.reduce((i, j) => i + j.price * j.amount, 0);
+  }
+ 
+  close() {
+    this.modalCtrl.dismiss();
+  }
+ 
+  async checkout() {
+    // Confirmação da compra
+ 
+    let alert = await this.alertCtrl.create({
+      header: 'Obrigado pela preferência!',
+      message: 'Seu pedido chega em breve',
+      buttons: ['OK']
+    });
+    alert.present().then(() => {
+      this.modalCtrl.dismiss();
+    });
+  }
 }
