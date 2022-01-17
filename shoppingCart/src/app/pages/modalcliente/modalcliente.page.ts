bc8a33c10acb9d12d78f62fd28c7e0ee703a39d8
Paulo Vitor Paixao <paulovitorpro021@gmail.com>
2022-01-17 19:23:01 -0300

    att - crud completo

--- shoppingCart/src/app/pages/modalcliente/modalcliente.page.ts
+++ shoppingCart/src/app/pages/modalcliente/modalcliente.page.ts
@@ -13,12 +13,21 @@ import { CadastroService, Cliente } from 'src/app/services/cadastro.service';
 })
 export class ModalclientePage implements OnInit {
 @Input() c: Cliente;
+atualizar = false;
+dados = {
+  nome: '',
+  cidade: '',
+  email: ''
+}
   constructor(private modalCtrl: ModalController,
               private service: CadastroService) { }
 
   ngOnInit() {
-  console.log("entrou no atualizar");
-  console.log(this.c);
+    if(this.c){
+      //console.log("Atualizar");
+      this.atualizar = true;
+      this.dados = this.c;
+    }
   }
 
 
@@ -30,9 +39,15 @@ export class ModalclientePage implements OnInit {
     //teste de cadastro exibido no log.
    //console.log(form.value);
    const cliente = form.value
+   if(this.atualizar){
+    this.service.update(cliente, this.c.id).subscribe(response =>{
+      this.modalCtrl.dismiss(response);
+    })
+   }else{
    this.service.create(cliente).subscribe(response =>{
      this.modalCtrl.dismiss(response);
    })
   }
 
+ }
 }
